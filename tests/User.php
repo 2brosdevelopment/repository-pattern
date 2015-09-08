5ef8f889d9fab2438d353ae1e44aecc9d3f94e15
Vincent Sposato <vincent.sposato@gmail.com>
2015-09-07 20:52:43 -0400

    Fixed User model to remove unnecessary things for testing

--- tests/User.php
+++ tests/User.php
@@ -2,20 +2,10 @@
 
     namespace TwoBros\RepositoryPattern\Tests;
 
-    use Illuminate\Auth\Authenticatable;
-    use Illuminate\Auth\Passwords\CanResetPassword;
-    use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-    use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-    use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
     use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Foundation\Auth\Access\Authorizable;
 
-    class User extends Model implements AuthenticatableContract,
-        AuthorizableContract,
-        CanResetPasswordContract
+    class User extends Model
     {
-
-        use Authenticatable, Authorizable, CanResetPassword;
 
         /**
          * The database table used by the model.
